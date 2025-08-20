4eb8b2c9b36e1e24d2a4e95b1e9ac84539cb39b7
Aniket Gursal <155827033+aniketgursal45@users.noreply.github.com>
2025-08-20 00:55:32 -0700

    Update 7a1.c

--- C/Assignment 1/7a1.c
+++ C/Assignment 1/7a1.c
@@ -1,6 +1,3 @@
-//Calculating total salary based on basic. If basic <=5000 da, ta and hra will be
-//10%,20% and 25% respectively otherwise da, ta and hra will be 15%,25% and 30%
-//respectively.
 #include<stdio.h>
 void main(){
 	int basic =6000;
@@ -21,5 +18,6 @@ void main(){
 		TS = basic + DA + TA + HRA;
 		printf(" Total Salary is:- %d",TS);
 	}
+
 
 }

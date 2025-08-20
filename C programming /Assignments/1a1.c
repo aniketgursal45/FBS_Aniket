df1e79612db443bc13a635279ca7d91a7f0e5463
Aniket Gursal <155827033+aniketgursal45@users.noreply.github.com>
2025-08-19 23:34:36 -0700

    Add files via upload

---
+++ C programming /Assignments/1a1.c
@@ -0,0 +1,12 @@
+//Write a program to check whether a number is even or odd.
+
+#include<stdio.h>
+void main(){
+	int no = 8;
+	
+	if(no % 2 ==0){
+		printf("Number is even");
+	}else{
+		printf("Number is odd");
+	}
+}
